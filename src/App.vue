b90135ef781435571b5669063262df0fbe98635e
Fadel442 <fadelmuhammad4402@gmail.com>
2024-10-14 19:28:07 +0700

    seconf commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Block from './components/komponen/Block.vue'
+import Result from './components/komponen/Result.vue';
+</script>
+
+<template>
+  <h1>Reaction Timer Game</h1>
+  <button @click="start" :disabled="isPlay">Click to Play!</button>
+  <Block v-if="isPlay" :delay="delay" @end="endGame"/>
+  <Result v-if="showResult" :score="score" />
+  <!-- <p>Reaction Time: {{ score }} ms</p> -->
+</template>
+
+<script>
+  export default {
+    components: { Block, Result },
+    data() {
+      return {
+        showResult: false,
+        isPlay: false,
+        delay: null,
+        score: null,
+      }
+    },
+    methods: {
+      start() {
+        this.delay = 2000 + Math.random() * 5000
+        this.isPlay = true
+        this.showResult= false
+      },
+      endGame(reactionTime) {
+        this.score = reactionTime
+        this.isPlay= false
+        this.showResult= true
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/komponen/Result.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h1>Reaction Time Scores</h1>
+        <h1 style="border: 2px dotted black; margin: 20px;">{{ score }}</h1>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['score']
+}
+</script>
+
+<style>
+    .container {
+        border: 2px dashed black;
+        margin: 50px 200px;
+        background-color: pink;
+    }
+</style>
