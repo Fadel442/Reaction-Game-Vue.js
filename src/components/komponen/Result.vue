4af97155c9f04e5412268526b65d9d81719a34f1
Fadel442 <fadelmuhammad4402@gmail.com>
2024-10-14 19:38:46 +0700

    finish

--- src/components/komponen/Result.vue
+++ src/components/komponen/Result.vue
@@ -1,14 +1,28 @@
 <template>
     <div class="container">
         <h1>Reaction Time Scores</h1>
-        <h1 style="border: 2px dotted black; margin: 20px;">{{ score }}</h1>
-
+        <h1 style="border: 2px dotted black; margin: 20px;">{{ score }} ms</h1>
+        <p>{{ rank }}</p>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['score']
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'I am The Flash'
+        } else if (this.score < 400) {
+            this.rank = 'I am Gundala'
+        } else {
+            this.rank = 'I am The Arrow'
+        }
+    },
 }
 </script>
 
